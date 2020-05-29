98ae44780a05aedb99d8d18022a44e648ff362a7
James McClarty <mariobud@gmail.com>
2020-05-29 13:20:30 -0500

    Commiting

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 
 function App() {
   return (
+    //Practicing connection
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
